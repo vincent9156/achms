111e4a04c32da68d38d6fb327dc901d9f43b3b55
saidul.04@gmail.com <saidul.04@gmail.com@34c1f756-d07e-b75f-3e9b-1e728fdd4fce>
2012-01-07 19:43:40 +0000

    Test

--- trunk/Hospital Management System/TestProject/Form1.Designer.cs
+++ trunk/Hospital Management System/TestProject/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCreateDB = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCreateDB
+            // 
+            this.btnCreateDB.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateDB.Name = "btnCreateDB";
+            this.btnCreateDB.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateDB.TabIndex = 0;
+            this.btnCreateDB.Text = "CreateDB";
+            this.btnCreateDB.UseVisualStyleBackColor = true;
+            this.btnCreateDB.Click += new System.EventHandler(this.btnCreateDB_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(370, 292);
+            this.Controls.Add(this.btnCreateDB);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreateDB;
     }
 }
 
